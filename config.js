113c161bf8eb7344cf41fb675c5d3c5010a00236
truongnlt99-spec <truongnlt99@gmail.com>
2026-07-17 09:25:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.CS_TOOL_CONFIG = {
   CLIENT_ID: '621765385511-nn699q39euvcf2slmqbv9jp6oi70kpgb.apps.googleusercontent.com',
 
   // URL Web App sau khi deploy Apps Script (kết thúc bằng /exec)
-  API_URL: 'https://script.google.com/macros/s/AKfycbxOSJ9L855wojNES4h8uu-frb1iwkUbaU8MhMKg-beh7sWq-cvAWLRzfqgGGnBc3y-w/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxV832pLcXGrT3F9rznq-cS3rD-ztJFhTLMXMuRWbDN5WgRnZ68ohlEA7e_eWCtr2qT/exec',
 
   // Domain email được phép đăng nhập
   ALLOWED_DOMAIN: 'base.vn',
